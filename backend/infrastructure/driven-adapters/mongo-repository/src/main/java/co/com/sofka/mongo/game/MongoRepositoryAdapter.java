963eb21e91012881e76e77475a222512af241847
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 14:40:06 -0500

    Se agrega adaptador que implementa las operaciones del repositorio

---
+++ backend/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/game/MongoRepositoryAdapter.java
@@ -0,0 +1,30 @@
+package co.com.sofka.mongo.game;
+
+import co.com.sofka.model.game.Game;
+import co.com.sofka.model.game.gateways.GameRepository;
+import co.com.sofka.model.player.Player;
+import co.com.sofka.mongo.helper.AdapterOperations;
+import org.reactivecommons.utils.ObjectMapper;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public class MongoRepositoryAdapter extends AdapterOperations<Game, GameDocument, String, MongoDBRepository>
+ implements GameRepository
+{
+
+    public MongoRepositoryAdapter(MongoDBRepository repository, ObjectMapper mapper) {
+        /**
+         *  Could be use mapper.mapBuilder if your domain model implement builder pattern
+         *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
+         *  Or using mapper.map with the class of the object model
+         */
+        super(repository, mapper, d -> mapper.map(d, Game.class));
+    }
+
+
+//    @Override
+    public Mono<Player> addPlayer(Player player) {
+        return this.addPlayer(player);
+    }
+}
