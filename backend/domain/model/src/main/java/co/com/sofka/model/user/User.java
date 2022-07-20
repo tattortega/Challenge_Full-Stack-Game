155c567505413e62bc371bdd954e63d0aabf6c2b
JEAR2 <jhedacro@gmail.com>
2022-07-20 09:34:01 -0500

    Se agrega Model User

---
+++ backend/domain/model/src/main/java/co/com/sofka/model/user/User.java
@@ -0,0 +1,14 @@
+package co.com.sofka.model.user;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class User {
+    private String id;
+    private String rol;
+}
