b26138b465afa275db6716422a2fd2e313e369b2
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 14:39:11 -0500

    Se agrega interfaz para el repositorio de mongo reactive

---
+++ backend/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/sofka/mongo/game/MongoDBRepository.java
@@ -0,0 +1,11 @@
+package co.com.sofka.mongo.game;
+
+import co.com.sofka.model.player.Player;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
+import reactor.core.publisher.Mono;
+
+public interface MongoDBRepository extends ReactiveMongoRepository<GameDocument, String>, ReactiveQueryByExampleExecutor<GameDocument> {
+
+//    Mono<Player> addPlayer(Player player);
+}
