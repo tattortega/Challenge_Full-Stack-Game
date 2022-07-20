62f20ba5e9eb4da56075cb733c5388a9b7ffc62e
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 10:36:16 -0500

    Agregando metodo para insertar jugador al juego

--- backend/domain/model/src/main/java/co/com/sofka/model/game/gateways/GameRepository.java
+++ backend/domain/model/src/main/java/co/com/sofka/model/game/gateways/GameRepository.java
@@ -1,12 +1,14 @@
 package co.com.sofka.model.game.gateways;
 
 import co.com.sofka.model.game.Game;
+import co.com.sofka.model.player.Player;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface GameRepository {
 
     Mono<Game> save(Game game);
+    Mono<Player> addPlayer(Player player);
     Flux<Game> findAll();
     Mono<Game> findById(String id);
     Mono<Void> deleteById(String id);
