d90bb05fedd64f2eb86c404ac1b3b70e0f019c0a
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 14:32:26 -0500

    Se quita el metodo de agregar jugador

--- backend/domain/model/src/main/java/co/com/sofka/model/game/gateways/GameRepository.java
+++ backend/domain/model/src/main/java/co/com/sofka/model/game/gateways/GameRepository.java
@@ -8,7 +8,7 @@ import reactor.core.publisher.Mono;
 public interface GameRepository {
 
     Mono<Game> save(Game game);
-    Mono<Player> addPlayer(Player player);
+//    Mono<Player> addPlayer(Player player);
     Flux<Game> findAll();
     Mono<Game> findById(String id);
     Mono<Void> deleteById(String id);
