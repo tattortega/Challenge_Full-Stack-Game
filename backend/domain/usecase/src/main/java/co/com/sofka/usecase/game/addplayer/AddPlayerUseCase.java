e838a1715ed90c81dd2c26dd5e8501934c08cba4
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 14:34:52 -0500

    Se modifica implementacion del metodo apply

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/addplayer/AddPlayerUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/addplayer/AddPlayerUseCase.java
@@ -8,26 +8,43 @@ import co.com.sofka.usecase.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 
 @RequiredArgsConstructor
-public class AddPlayerUseCase implements BiFunction<Game, Player, Mono<Player>> {
+public class AddPlayerUseCase implements BiFunction<Game, Player, Mono<Object>> {
 
     private final PlayerRepository playerRepository;
     private final GameRepository gameRepository;
 
 
     @Override
-    public Mono<Player> apply(Game game,Player player) {
-        return playerRepository.findById(player.getId())
-                .switchIfEmpty(Mono.error(new NotFoundException("No se encontro jugador")))
-//                .map(player1 -> player1.toBuilder()
-//                        .id(player.getId())
-//                        .cards(player.getCards())
-//                        .score(player.getScore())
-//                        .user(player.getUser())
-//                        .build())
-                .flatMap(gameRepository::addPlayer);
+    public Mono<Object> apply(Game game, Player player) {
+        Set<Player> playerSet = game.getPlayers();
+//        return playerRepository.findById(player.getId())
+//                .switchIfEmpty(Mono.error(new NotFoundException("No se encontro jugador")))
+////                .map(player1 -> player1.toBuilder()
+////                        .id(player.getId())
+////                        .cards(player.getCards())
+////                        .score(player.getScore())
+////                        .user(player.getUser())
+////                        .build())
+//                .map(player1 -> {
+//                    playerSet.add(player1);
+//                    return game.setPlayers(playerSet);
+//                });
+        Mono<Player> playername = playerRepository.findById(player.getId());
+        playerSet.add(playername.block());
+        return gameRepository.findById(game.getId())
+                .switchIfEmpty(Mono.error(new NotFoundException("No se encontro juego")))
+                .map(game1 -> game1.toBuilder()
+                        .id(game.getId())
+                        .players(playerSet)
+                        .board(game.getBoard())
+                        .round(game.getRound())
+                        .cards(game.getCards())
+                        .build())
+                .flatMap(gameRepository::save);
     }
 }
