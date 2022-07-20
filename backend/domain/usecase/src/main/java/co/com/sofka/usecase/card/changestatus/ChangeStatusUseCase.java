9c89c31339c5796afe51261757b183588e5d39c7
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-20 10:51:14 -0500

    Se agrega caso de uso para el cambio de estado de la tarjeta

---
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/card/changestatus/ChangeStatusUseCase.java
@@ -0,0 +1,11 @@
+package co.com.sofka.usecase.card.changestatus;
+
+import co.com.sofka.model.card.Card;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class ChangeStatusUseCase{
+
+}
