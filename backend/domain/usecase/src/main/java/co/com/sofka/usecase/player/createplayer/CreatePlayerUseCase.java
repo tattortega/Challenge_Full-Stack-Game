5e4c240f2f671002762b81c894cdc2d9b735b3ae
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 14:34:02 -0500

    Se corrige metodo apply

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/player/createplayer/CreatePlayerUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/player/createplayer/CreatePlayerUseCase.java
@@ -1,6 +1,18 @@
 package co.com.sofka.usecase.player.createplayer;
 
+import co.com.sofka.model.player.Player;
+import co.com.sofka.model.player.gateways.PlayerRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
 @RequiredArgsConstructor
-public class CreatePlayerUseCase {
+public class CreatePlayerUseCase implements Function<Player, Mono<Player>> {
+
+    private final PlayerRepository playerRepository;
+    @Override
+    public Mono<Player> apply(Player player) {
+        return playerRepository.save(player);
+    }
 }
