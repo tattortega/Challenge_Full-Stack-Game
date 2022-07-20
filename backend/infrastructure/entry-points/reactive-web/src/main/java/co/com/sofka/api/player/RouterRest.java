67a655b57f2dc1a6d2131c4f778b01209fff6217
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 15:00:15 -0500

    Se agrega build.gradle

---
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/player/RouterRest.java
@@ -0,0 +1,21 @@
+//package co.com.sofka.api.player;
+//
+//import co.com.sofka.api.player.handler.CreatePlayerHandler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//
+//import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+//import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+//
+//
+//@Configuration
+//public class RouterRest {
+//
+//
+//    @Bean
+//    public RouterFunction<ServerResponse> routerCreatePlayerFunction(CreatePlayerHandler createPlayerHandler) {
+//        return route(POST("/api/player"), createPlayerHandler::createPlayer);
+//    }
+//}
