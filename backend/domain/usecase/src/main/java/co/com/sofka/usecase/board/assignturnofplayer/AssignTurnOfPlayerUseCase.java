76d78cb5ff49c17f60c9ee1f9dd8682ecc91f6aa
JEAR2 <jhedacro@gmail.com>
2022-07-20 11:10:20 -0500

    Se agregan casos de uso para asignar turno y controlar tiempo de jugada en el tablero

---
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/board/assignturnofplayer/AssignTurnOfPlayerUseCase.java
@@ -0,0 +1,6 @@
+package co.com.sofka.usecase.board.assignturnofplayer;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class AssignTurnOfPlayerUseCase {
+}
---
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/board/validatetimeturn/ValidateTimeTurnUseCase.java
@@ -0,0 +1,6 @@
+package co.com.sofka.usecase.board.validatetimeturn;
+
+import lombok.RequiredArgsConstructor;
+@RequiredArgsConstructor
+public class ValidateTimeTurnUseCase {
+}
