e4427f25783ab3b021498da391dd8e4429a3196a
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-20 14:59:11 -0500

    Se agrega el handler para el caso de uso crear jugador

---
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/player/handler/CreatePlayerHandler.java
@@ -0,0 +1,29 @@
+package co.com.sofka.api.player.handler;
+
+import co.com.sofka.model.game.Game;
+import co.com.sofka.model.player.Player;
+import co.com.sofka.usecase.player.createplayer.CreatePlayerUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class CreatePlayerHandler {
+
+    private final CreatePlayerUseCase createPlayerUseCase;
+
+    public Mono<ServerResponse> createPlayer(ServerRequest serverRequest) {
+        return serverRequest
+                .bodyToMono(Player.class)
+                .flatMap(createPlayerUseCase::apply)
+                .flatMap(player -> ServerResponse
+                        .status(HttpStatus.CREATED)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(player));
+    }
+}
