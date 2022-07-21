3d47ff898acc3d53fb2a7ce7e90c22b153120252
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-21 13:39:00 -0500

    Se agrega caso de uso para iniciar juego implementando metodo apply

---
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/startgame/StartGameUseCase.java
@@ -0,0 +1,24 @@
+package co.com.sofka.usecase.game.startgame;
+
+import co.com.sofka.model.card.Card;
+import co.com.sofka.model.game.Game;
+import co.com.sofka.model.game.gateways.GameRepository;
+import co.com.sofka.usecase.game.distributecards.DistributeCardsUseCase;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class StartGameUseCase implements Function<Game, Mono<Game>> {
+
+    private final GameRepository gameRepository;
+
+    @Override
+    public Mono<Game> apply(Game game) {
+        return gameRepository.findById(game.getId())
+                .map(p -> new Game(game.getId(), game.getRound(), game.getPlayers(), game.getCards()))
+                .flatMap(gameRepository::save);
+    }
+}
