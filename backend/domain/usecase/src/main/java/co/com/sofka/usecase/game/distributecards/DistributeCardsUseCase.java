ac94b6f38c1736de196c76dfb21f459eeb819453
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-21 13:41:04 -0500

    Se corrigen metodos para distribuir cartas

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/distributecards/DistributeCardsUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/distributecards/DistributeCardsUseCase.java
@@ -11,6 +11,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class DistributeCardsUseCase implements Function<Game, Mono<Game>> {
@@ -21,30 +22,44 @@ public class DistributeCardsUseCase implements Function<Game, Mono<Game>> {
 
     @Override
     public Mono<Game> apply(Game game) {
-        Set<Card> cardSet = game.getCards();
-        return gameRepository.findById(game.getId())
-                .map(p -> new Game(game.getId(), game.getRound(), game.getPlayers(), game.getCards()))
-                .doOnNext(game1 ->
-                        game1.getPlayers().iterator().next().setCards(assignCardsToPlayer(cardSet, game)));
-    }
-
-    private Set<Card> assignCardsToPlayer(Set<Card> cards, Game game) {
-        Set<Card> cardSet = new HashSet<>();
+        System.out.println("entro");
+        Set<Player> playerSet1 = new HashSet<>();
         Set<Player> playerSet = game.getPlayers();
-        Flux.fromIterable(playerSet).map(player-> player.toBuilder()
-                .id(player.getId())
-                .user(player.getUser())
-                .score(player.getScore())
-                .cards(addCards(cards, game))
-                .build())
+        playerSet1 = playerSet.stream().map(player -> {
+            Set<Card> cardSet = addCards(game);
+            return player.toBuilder()
+                    .id(player.getId())
+                    .score(player.getScore())
+                    .user(player.getUser())
+                    .cards(cardSet)
+                    .build();
+        }).collect(Collectors.toSet());
+        Set<Player> finalPlayerSet = playerSet1;
+        Flux.fromIterable(finalPlayerSet).map(player -> {
+                    Set<Card> cardSet = addCards(game);
+                    return player.toBuilder()
+                            .id(player.getId())
+                            .user(player.getUser())
+                            .score(player.getScore())
+                            .cards(cardSet)
+                            .build();
+                })
                 .flatMap(playerRepository::save)
                 .subscribe();
-        return cardSet;
+        return gameRepository.findById(game.getId())
+                .map(p -> p.toBuilder()
+                        .id(game.getId())
+                        .round(game.getRound())
+                        .players(finalPlayerSet)
+                        .cards(game.getCards())
+                        .build())
+                .flatMap(gameRepository::save);
     }
 
-    private Set<Card> addCards(Set<Card> cards, Game game) {
+    private Set<Card> addCards(Game game) {
+        System.out.println("entro2");
         Set<Card> cardSet = new HashSet<>();
-        List<Card> cardList = new ArrayList<>(cards);
+        List<Card> cardList = new ArrayList<>(game.getCards());
 
         Random r = new Random();
         Flux.range(0, 5)
@@ -57,24 +72,26 @@ public class DistributeCardsUseCase implements Function<Game, Mono<Game>> {
     }
 
     private Mono<Game> removeCard(Card card, Game game) {
-        System.out.println("entro3");
         Set<Card> cardList = new HashSet<>(game.getCards());
-        System.out.println("lista original"+cardList);
+        System.out.println("lista original" + cardList);
         cardList.remove(card);
-        System.out.println("lista cambiada"+cardList);
+        System.out.println("lista cambiada" + cardList);
 //        return Mono.just(game.toBuilder()
 //                .id(game.getId())
 //                .players(game.getPlayers())
 //                .round(game.getRound())
 //                .cards(cardList)
 //                .build());
-        Game gameMono = new Game(game.getId(), game.getRound(), game.getPlayers(), cardList);
-        System.out.println(gameMono.getPlayers());
-        return gameRepository.save(gameMono);
-//        return gameRepository.findById(game.getId())
-//                .map(p -> )
-//                .log()
-//                .flatMap(gameRepository::save);
+//        Game gameMono = new Game(game.getId(), game.getRound(), game.getPlayers(), cardList);
+//        return gameRepository.save(gameMono);
+        return gameRepository.findById(game.getId())
+                .map(p -> p.toBuilder()
+                        .id(game.getId())
+                        .round(game.getRound())
+                        .players(game.getPlayers())
+                        .cards(cardList)
+                        .build())
+                .flatMap(gameRepository::save);
     }
 }
 
