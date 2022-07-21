7955d4be2029514acb4521b35ca9d529470ad653
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-21 13:40:32 -0500

    Se agrega handler para el caso de uso iniciar juego invocando los casos de uso

---
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/game/handler/StartGameHandler.java
@@ -0,0 +1,33 @@
+package co.com.sofka.api.game.handler;
+
+import co.com.sofka.model.game.Game;
+import co.com.sofka.usecase.game.creategame.CreateGameUseCase;
+import co.com.sofka.usecase.game.distributecards.DistributeCardsUseCase;
+import co.com.sofka.usecase.game.startgame.StartGameUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class StartGameHandler {
+
+    private final StartGameUseCase startGameUseCase;
+    private final DistributeCardsUseCase distributeCardsUseCase;
+
+    public Mono<ServerResponse> startGame(ServerRequest serverRequest) {
+        return serverRequest
+                .bodyToMono(Game.class)
+                .flatMap(startGameUseCase::apply)
+                .flatMap(distributeCardsUseCase::apply)
+                .flatMap(game -> ServerResponse
+                        .status(HttpStatus.CREATED)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(game));
+    }
+
+}
