b9bdec1cf799ca38aa1fb5ead73817e702c69ee2
JEAR2 <jhedacro@gmail.com>
2022-07-21 16:02:31 -0500

    se cambia el nombre RouteRest a RoutePlayerRest

--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/player/RouterPlayerRest.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/player/RouterPlayerRest.java
@@ -1,21 +1,29 @@
-//package co.com.sofka.api.player;
-//
-//import co.com.sofka.api.player.handler.CreatePlayerHandler;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.reactive.function.server.RouterFunction;
-//import org.springframework.web.reactive.function.server.ServerResponse;
-//
-//import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-//import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-//
-//
-//@Configuration
-//public class RouterRest {
-//
-//
-//    @Bean
-//    public RouterFunction<ServerResponse> routerCreatePlayerFunction(CreatePlayerHandler createPlayerHandler) {
-//        return route(POST("/api/player"), createPlayerHandler::createPlayer);
-//    }
-//}
+package co.com.sofka.api.player;
+
+import co.com.sofka.api.game.handler.GetPlayerHandler;
+import co.com.sofka.api.player.handler.CreatePlayerHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+
+@Configuration
+public class RouterPlayerRest {
+
+
+    @Bean
+    public RouterFunction<ServerResponse> routerCreatePlayerFunction(CreatePlayerHandler createPlayerHandler) {
+        return route(POST("/api/player"), createPlayerHandler::createPlayer);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerGetPlayerFunction(GetPlayerHandler getPlayerHandler) {
+        return route(GET("/api/player/{id}"), getPlayerHandler::getPlayer);
+    }
+
+}
