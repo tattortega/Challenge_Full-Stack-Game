4ea64d4949b7506a8c2ae5d6d1b0d8a4dc506cc6
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-21 00:07:09 -0500

    Se modifica rutas del router

--- backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/game/RouterRest.java
+++ backend/infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/game/RouterRest.java
@@ -1,13 +1,19 @@
 package co.com.sofka.api.game;
 
-import co.com.sofka.api.game.handler.AddPlayerHandler;
-import co.com.sofka.api.game.handler.CreateGameHandler;
+import co.com.sofka.api.game.handler.*;
 import co.com.sofka.api.player.handler.CreatePlayerHandler;
+import co.com.sofka.model.game.gateways.GameRepository;
+import co.com.sofka.usecase.game.creategame.CreateGameUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
@@ -17,7 +23,9 @@ public class RouterRest {
 
     @Bean
     public RouterFunction<ServerResponse> routerCreateGameFunction(CreateGameHandler createGameHandler) {
-        return route(POST("/api/game"), createGameHandler::createGame);
+        return RouterFunctions.route(
+                POST("/api/game").and(contentType(APPLICATION_JSON)), createGameHandler::createGame);
+//        return route(POST("/api/game").and(contentType(APPLICATION_JSON)), createGameHandler::createGame);
     }
 
     @Bean
@@ -29,4 +37,20 @@ public class RouterRest {
     public RouterFunction<ServerResponse> routerCreatePlayerFunction(CreatePlayerHandler createPlayerHandler) {
         return route(POST("/api/player"), createPlayerHandler::createPlayer);
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerGetPlayerFunction(GetPlayerHandler getPlayerHandler) {
+        return route(GET("/api/player/{id}"), getPlayerHandler::getPlayer);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerGetGameFunction(GetGameHandler getGameHandler) {
+        return route(GET("/api/game/{id}"), getGameHandler::getGame);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> routerDistributeCardsFunction(DistributeCardsInGameHandler distributeCardsInGameHandler) {
+        return route(POST("/api/game/cards"), distributeCardsInGameHandler::distributeCards);
+    }
+
 }
