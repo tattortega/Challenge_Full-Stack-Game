67e0ed9463a134e4f21133ea0dd130e45cdd827f
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-23 12:29:10 -0500

    Se implementa funcionalidad para el caso de uso que remueve a los jugadores que ya no tienen cartas dentro del juego

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/removeplayer/RemovePlayerUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/removeplayer/RemovePlayerUseCase.java
@@ -1,6 +1,31 @@
 package co.com.sofka.usecase.game.removeplayer;
 
+import co.com.sofka.model.game.Game;
+import co.com.sofka.model.game.gateways.GameRepository;
+import co.com.sofka.model.player.Player;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
-public class RemovePlayerUseCase {
+public class RemovePlayerUseCase implements BiFunction<Game, Set<Player>, Mono<Game>> {
+
+    private final GameRepository gameRepository;
+
+    @Override
+    public Mono<Game> apply(Game game, Set<Player> playersRemove) {
+
+        Set<Player> players = playersRemove;
+
+        players.stream().map(player -> {
+            game.getPlayers().remove(player);
+            return game;
+        }).collect(Collectors.toSet());
+
+        return gameRepository.save(game);
+    }
 }
