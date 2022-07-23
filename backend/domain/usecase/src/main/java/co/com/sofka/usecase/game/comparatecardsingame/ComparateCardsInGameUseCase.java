2cfd7bb3e341343f562d2d3abcf4f88fbb7bdc0d
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-23 10:33:09 -0500

    Se implementa funcionalidad del caso de uso que compara las cartas en el juego para validar la ganadora

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/comparatecardsingame/ComparateCardsInGameUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/comparatecardsingame/ComparateCardsInGameUseCase.java
@@ -1,6 +1,34 @@
 package co.com.sofka.usecase.game.comparatecardsingame;
 
+import co.com.sofka.model.card.Card;
+import co.com.sofka.model.game.Game;
+import co.com.sofka.model.game.gateways.GameRepository;
+import co.com.sofka.model.player.Player;
+import co.com.sofka.usecase.game.assignwinnerround.AssignWinnerRoundUseCase;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
-public class ComparateCardsInGameUseCase {
+public class ComparateCardsInGameUseCase implements Function<Game, Mono<Game>> {
+
+    private final GameRepository gameRepository;
+    private final AssignWinnerRoundUseCase assignWinnerRoundUseCase;
+
+    @Override
+    public Mono<Game> apply(Game game) {
+
+        Map<String, Card> mapCards = game.getBoard().getCardsBetPlayers();
+
+        String idWinner = mapCards.entrySet()
+                .stream()
+                .max((a, b) -> a.getValue().getPower().compareTo(b.getValue().getPower()))
+                .map(Map.Entry::getKey).get();
+
+        return assignWinnerRoundUseCase.apply(game, idWinner);
+    }
 }
