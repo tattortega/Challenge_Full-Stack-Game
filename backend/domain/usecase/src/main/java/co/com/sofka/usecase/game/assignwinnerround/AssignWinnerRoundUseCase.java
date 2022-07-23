bfe87e7f0f817ff7b85f76bd0fb5b5471ebf4fe0
Oscar Farfan <oscarfarfan92@gmail.com>
2022-07-23 10:31:41 -0500

    Se implementa funcionalidad del caso de uso que asigna el ganador de la ronda

--- backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/assignwinnerround/AssignWinnerRoundUseCase.java
+++ backend/domain/usecase/src/main/java/co/com/sofka/usecase/game/assignwinnerround/AssignWinnerRoundUseCase.java
@@ -1,6 +1,38 @@
 package co.com.sofka.usecase.game.assignwinnerround;
 
+import co.com.sofka.model.card.Card;
+import co.com.sofka.model.game.Game;
+import co.com.sofka.model.game.gateways.GameRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
-public class AssignWinnerRoundUseCase {
+public class AssignWinnerRoundUseCase implements BiFunction<Game, String, Mono<Game>> {
+
+    private final GameRepository gameRepository;
+
+    @Override
+    public Mono<Game> apply(Game game, String idWinner) {
+
+        Map<String, Card> mapCards = game.getBoard().getCardsBetPlayers();
+
+        Set<Card> betCards = mapCards.entrySet().stream().map(Map.Entry::getValue).collect(Collectors.toSet());
+
+        Flux.fromIterable(game.getPlayers())
+                .filter(player -> player.getId().equals(idWinner))
+                .map(player -> betCards.stream()
+                        .map(card -> {
+                            player.getCards().add(card);
+                            return player;
+                        }).collect(Collectors.toList()))
+                .subscribe();
+
+        return gameRepository.save(game);
+    }
 }
